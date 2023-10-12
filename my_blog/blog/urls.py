34e9e0d03e474176ea4c0d197d67e720469e5905
blacktooth <facuurdev@gmail.com>
2023-10-12 01:44:53 -0300

    added Atom and Rss feeds

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,8 +1,13 @@
 from . import views
 from django.urls import path
+from .feeds import LatestPostsFeed
+
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),#Se crea una ruta para la lista de post
     path("<slug:slug>/", views.post_detail, name="post_detail"),#Se crea una ruta para el detalle del post
+    path("feed/rss", LatestPostsFeed(), name="post_feed"),#Se crea una ruta para el feed de los post
     
+    
+
 ]
